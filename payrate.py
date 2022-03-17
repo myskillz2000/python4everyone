51f22a0d21bd0dd5df68cb5683bfb2bee24c8248
Matt Stiller <MStiller@ocslog.com>
2022-03-17 12:14:56 -0400

    Restructured the files.

--- payrate.py
+++
@@ -1,7 +0,0 @@
-hours = 35.0 # Assignment
-rate = 12.50 # Assignment
-pay = hours * rate # Calculation of assigned variables
-
-nam = input("Who are you?")
-print('Welcome ', nam)
-print("You will be payed $",pay)
