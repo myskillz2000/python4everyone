d96c5921034ca11d5d463ab430c41c0524150fd6
Matthew Stiller <30756911+myskillz2000@users.noreply.github.com>
2022-03-17 12:56:26 -0400

    First function made for languages.

---
+++ Chapter 4/language.py
@@ -0,0 +1,9 @@
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+
+greet(input('What language do you speak?'))
